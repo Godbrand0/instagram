63abaa16157abb580c9858dc124ea95c3e997a7a
godbrand <thompsoneregha005@gmail.com>
2024-12-16 05:15:40 -0800

    starter pack

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Feed from "./components/Feed";
+import FollowSection from "./components/FollowSection";
+import NavBar from "./components/NavBar";
+import StoryBoard from "./components/StoryBoard";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+      <div className="body">
+        <div>
+          <StoryBoard />
+          <Feed />
+        </div>
+
+        <FollowSection />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/FollowSection.jsx
+++ src/components/FollowSection.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import "../App.css";
 
 export default function FollowSection() {
-  return <div></div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: "GET",
+        url: "https://instagram-scraper-api2.p.rapidapi.com/v1/followers",
+        params: {
+          username_or_id_or_url: "mrbeast",
+        },
+        headers: {
+          "x-rapidapi-key":
+            "d69dcb98e0msh1f3c5c95ebbc67ep146c90jsn3e9bf6fdffd3",
+          "x-rapidapi-host": "instagram-scraper-api2.p.rapidapi.com",
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return <div className="follow_section"> the follow section</div>;
 }
